343103f6bca1392491b39792648b6fe646b25c79
sharvaree17 <97276843+sharvaree17@users.noreply.github.com>
2022-02-23 19:24:33 +0530

    area of circle
    This program will print 
    Input the radius of the circle : 1.1 The area of the circle with radius 1.1 is: 3.8013271108436504

---
+++ AreaOfCircle.py
@@ -0,0 +1,5 @@
+Radius = float(input("Input the radius of the circle : "))
+Area_of_the_circle = (22/7)*(Radius**2)
+
+print("The output of the circle with radius {} is {}".format(Radius,Area_of_the_circle))
+
